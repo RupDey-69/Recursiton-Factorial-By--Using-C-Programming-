73dda75e30485d01b4cc9706907c41a99b8efb6c
RupDey <rupdey2006@gmail.com>
2025-11-30 17:02:56 +0530

    Update FactorialByUsing-Recursion.c

--- FactorialByUsing-Recursion.c
+++ FactorialByUsing-Recursion.c
@@ -1,28 +1,3 @@
-// #include <stdio.h>
-// int fact(int n)
-// {
-
-//     if (n == 0 || n == 1)
-//         return 1;
-// return n * fact(n - 1);
-// }
-
-// int main()
-// {
-
-//     int n;
-//     printf("Enter the Number:");
-//     scanf("%d", &n);
-
-//     for (int i = 1; i <= n; i++)
-//     {
-
-//         printf("%d", fact(i));
-//     }
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 int fact(int n)
@@ -46,3 +21,4 @@ int main()
 
     return 0;
 }
+
